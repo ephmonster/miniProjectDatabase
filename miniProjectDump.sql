54e881081e35191bcd171c510fcf4ec2b52bf41d
ephraim <ephraimungar6@gmail.com>
2024-06-05 22:40:41 +0300

    add files using lfs

--- miniProjectDump.sql
+++ miniProjectDump.sql
@@ -1,1 +1,3 @@
-will upload for future stage, not required for this stage
+version https://git-lfs.github.com/spec/v1
+oid sha256:dd6280f07846956e94621d1d46e032b710f4ebf7bbc2051f48d8b511f98f183d
+size 59
