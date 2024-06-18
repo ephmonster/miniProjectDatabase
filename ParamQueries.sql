aae357e2d47f6224f53b91d8150561d8e5390b4d
ephraim <ephraimungar6@gmail.com>
2024-06-18 15:12:36 +0300

    added execute

--- ParamQueries.sql
+++ ParamQueries.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:88eef1f31cdb3cf5dba90477ee84a18d7dda487e4961a27727b0257cf5778190
-size 738
+oid sha256:c87259de21eb1c4ccfa32a746304f7955343cd8090d1058d7c8be6c384a4d3ca
+size 971
