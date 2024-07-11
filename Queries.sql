b84f953222efe0511dd52c2096510cbcfb0f9f10
ephraim <ephraimungar6@gmail.com>
2024-07-11 21:55:34 +0300

    new queries

--- Queries.sql
+++ Queries.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b7ad5a7db058f7ee72b0f1a87dd1456523ef4ea6ba14ce76723e1ad46c3db105
-size 730
+oid sha256:f975a926b8832d56d8d4ee790aa6f7ce70aa237e2209389160022f4752b5192d
+size 1179
