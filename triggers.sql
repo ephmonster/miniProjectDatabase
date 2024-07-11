7dba56e4f7b1146fabf689666f307d46499aedc5
ephraim <ephraimungar6@gmail.com>
2024-07-11 21:42:02 +0300

    Track large SQL files with Git LFS

---
+++ triggers.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6ccbf81e4fb2d57b78134c4d1d3e01c61b29b23518158fe304ded56ef96fffc3
+size 1029
---
+++ view_queries.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ce329b097ffe716bd388b1a9cc99710f6cb08688fae407aa9217ac92d6690c6b
+size 2675
